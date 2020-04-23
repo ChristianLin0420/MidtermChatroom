d202c0fa64b14649c802ba72242fcef39daadd88
BJLin <crlc112358@gmail.com>
2020-04-23 23:39:30 +0800

    login interface design is completed except three external login method

---
+++ public/js/config.js
@@ -0,0 +1,13 @@
+// Initialize Firebase
+// TODO: Replace with your project's customized code snippet 
+var firebaseConfig = {
+    apiKey: "AIzaSyDRK8TCve3ddC8UjZ_x0q_C4Vq2CJME1Cw",
+    authDomain: "web-midterm-project-chatroom.firebaseapp.com",
+    databaseURL: "https://web-midterm-project-chatroom.firebaseio.com",
+    projectId: "web-midterm-project-chatroom",
+    storageBucket: "web-midterm-project-chatroom.appspot.com",
+    messagingSenderId: "317623170660",
+    appId: "1:317623170660:web:27285a59b3fc5e86d89171",
+    measurementId: "G-WQJMMJNTFV"
+};
+firebase.initializeApp(firebaseConfig);
